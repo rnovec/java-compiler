5d6eec58f7f0d38601dd6ab4ac83e0ebfc33939c
Raul Novelo <rnovelo.cruz98@gmail.com>
2020-10-27 21:58:35 -0600

    Updates

---
+++ precache-manifest.7c26e89fe642124e6ad46d711a9b8c60.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c920903fe86b600e53a01c4e49c611",
+    "url": "/java-compiler/bulma.min.css"
+  },
+  {
+    "revision": "1ebce4e2c5416e320db5",
+    "url": "/java-compiler/css/app.c07cc864.css"
+  },
+  {
+    "revision": "db5d2b474427c55f83a6a5eeb74aae43",
+    "url": "/java-compiler/favicon.svg"
+  },
+  {
+    "revision": "c2001819eb6f08e158bf2f39aadca820",
+    "url": "/java-compiler/index.html"
+  },
+  {
+    "revision": "1ebce4e2c5416e320db5",
+    "url": "/java-compiler/js/app.9b649b0f.js"
+  },
+  {
+    "revision": "340e5d193ac2828ace6a",
+    "url": "/java-compiler/js/chunk-vendors.2843d7cc.js"
+  },
+  {
+    "revision": "ae19eb2f162f1def19d584484a528095",
+    "url": "/java-compiler/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/java-compiler/robots.txt"
+  }
+]);
---
+++ precache-manifest.b3366f01d5558acceee371729d027de4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01693d09d7167edc105",
+    "url": "/java-compiler/css/app.c07cc864.css"
+  },
+  {
+    "revision": "728d51dedd003e626e574f24747ee938",
+    "url": "/java-compiler/index.html"
+  },
+  {
+    "revision": "d01693d09d7167edc105",
+    "url": "/java-compiler/js/app-legacy.8c2914bf.js"
+  },
+  {
+    "revision": "345bdcf3636f3e272aa8",
+    "url": "/java-compiler/js/chunk-vendors-legacy.97f4a323.js"
+  },
+  {
+    "revision": "ae19eb2f162f1def19d584484a528095",
+    "url": "/java-compiler/manifest.json"
+  }
+]);
