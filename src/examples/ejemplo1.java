ee25f7b8f2d6c24a78014bf5b448a6e3660ba7a6
Raul Novelo <rnovelo.cruz98@gmail.com>
2020-11-11 23:56:22 -0600

    Update exmaples for 3-Address Code generation

--- src/examples/ejemplo1.java
+++ src/examples/ejemplo1.java
@@ -1,7 +1,7 @@
 void prueba1 ()
 {
     a = a + b * 5;
-    while ( a % 2 == 0 && b == 2)
+    while ( a % 2 == 0 || b == 2)
     {
         a = a * 5;
     }
--- src/examples/ejemplo4.java
+++ src/examples/ejemplo4.java
@@ -1,7 +1,7 @@
 void prueba1 ()
 {
     int a = a + b + c * 5;
-    while ( y < 8)
+    while ( y % 2 == 0)
     {
         a = a * 5;
     }
