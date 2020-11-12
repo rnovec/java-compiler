d2d62522fae795f5fc7d13efc12c6d415403fa86
Raul Novelo <rnovelo.cruz98@gmail.com>
2020-11-12 13:22:02 -0600

    fix: api url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:8000/api/v1'
-      : 'https://compiler-api.herokuapp.comapi/v1',
+      : 'https://compiler-api.herokuapp.com/api/v1',
   timeout: 20000,
   mode: 'cors',
   withCredentials: false
